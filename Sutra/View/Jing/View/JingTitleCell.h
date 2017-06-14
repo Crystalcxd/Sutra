79b775d019e8bd8ef61a5bd14b6e79043605e899
Xiaodong <Micheal@sleepace.net>
2017-06-14 20:33:45 +0800

    UI控件

---
+++ Sutra/View/Jing/View/JingTitleCell.h
@@ -0,0 +1,13 @@
+//
+//  JingTitleCell.h
+//  Sutra
+//
+//  Created by Michael on 2017/6/14.
+//  Copyright © 2017年 com.medica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JingTitleCell : UITableViewCell
+
+@end
---
+++ Sutra/View/Jing/View/JingTitleCell.m
@@ -0,0 +1,24 @@
+//
+//  JingTitleCell.m
+//  Sutra
+//
+//  Created by Michael on 2017/6/14.
+//  Copyright © 2017年 com.medica. All rights reserved.
+//
+
+#import "JingTitleCell.h"
+
+@implementation JingTitleCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
