a5449bbd15b51d1d268b089185becbfd83d10aeb
Michael <michael@sleepace.net>
2017-06-14 09:58:58 +0800

    Initial Commit

---
+++ Sutra/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sutra
+//
+//  Created by medica_mac on 2017/6/14.
+//  Copyright © 2017年 com.medica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sutra/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sutra
+//
+//  Created by medica_mac on 2017/6/14.
+//  Copyright © 2017年 com.medica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
