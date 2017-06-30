47435331b87b74eaf3007f2fc171456c771a2403
Michael <michael@sleepace.net>
2017-06-30 15:57:34 +0800

    课 列表数据（单课）

--- Sutra/View/Ke/Controller/KeViewController.m
+++ Sutra/View/Ke/Controller/KeViewController.m
@@ -8,6 +8,8 @@
 
 #import "KeViewController.h"
 
+#import "KeAlbum.h"
+
 @interface KeViewController (){
     NSMutableArray *_Ke;
     NSMutableArray *_KeAlbum;
@@ -26,31 +28,36 @@
 
 - (void)creatKeData {
     _Ke=  [[NSMutableArray alloc] init];
+    [_Ke addObject:@{@"name":@"香赞",        @"url":@"b01xz.sutra"}];
+    [_Ke addObject:@{@"name":@"宝鼎赞",      @"url":@"b02bdz.sutra"}];
+    [_Ke addObject:@{@"name":@"楞严咒",      @"url":@"b03lyz.sutra"}];
+    [_Ke addObject:@{@"name":@"大悲咒",       @"url":@"b04dbz.sutra"}];
+    [_Ke addObject:@{@"name":@"十小咒",       @"url":@"b05sxz.sutra"}];
+    [_Ke addObject:@{@"name":@"心经",        @"url":@"b06xj.sutra"}];
+    [_Ke addObject:@{@"name":@"回向偈",       @"url":@"b07hxj.sutra"}];
+    [_Ke addObject:@{@"name":@"赞佛偈",       @"url":@"b08zfj.sutra"}];
+    [_Ke addObject:@{@"name":@"念圣号",       @"url":@"b09nsh.sutra"}];
+    [_Ke addObject:@{@"name":@"发愿文普贤十大愿王",       @"url":@"b10fyws.sutra"}];
+    [_Ke addObject:@{@"name":@"三皈依",       @"url":@"b11sgy.sutra"}];
+    [_Ke addObject:@{@"name":@"赞观音文",       @"url":@"b12zgyw.sutra"}];
+    [_Ke addObject:@{@"name":@"阿弥陀经",       @"url":@"b13amtj.sutra"}];
+    [_Ke addObject:@{@"name":@"礼佛大忏悔文",     @"url":@"b14dchw.sutra"}];
+    [_Ke addObject:@{@"name":@"蒙山施食",   @"url":@"b15msss.sutra"}];
+    [_Ke addObject:@{@"name":@"往生咒",         @"url":@"b16wsz.sutra"}];
+    [_Ke addObject:@{@"name":@"观闻圣号",       @"url":@"b17gwsh.sutra"}];
+    [_Ke addObject:@{@"name":@"发愿文慈云净土",       @"url":@"b18fywj.sutra"}];
+    [_Ke addObject:@{@"name":@"伽蓝赞",       @"url":@"b19qlz.sutra"}];
+    [_Ke addObject:@{@"name":@"普贤菩萨警众偈",       @"url":@"b20jzj.sutra"}];
+    [_Ke addObject:@{@"name":@"韦天赞",       @"url":@"b21wtz.sutra"}];
 
-    [_Ke addObject:@{@"name":@"宝鼎赞",     @"url":@"宝鼎赞.sutra"}];
-    [_Ke addObject:@{@"name":@"楞严咒",     @"url":@"楞严咒.sutra"}];
-    [_Ke addObject:@{@"name":@"大悲咒",       @"url":@"大悲咒.sutra"}];
-    [_Ke addObject:@{@"name":@"十小咒",       @"url":@"十小咒.sutra"}];
-    [_Ke addObject:@{@"name":@"心经",        @"url":@"课心经.sutra"}];
-    [_Ke addObject:@{@"name":@"回向偈",       @"url":@"回向偈.sutra"}];
-    [_Ke addObject:@{@"name":@"赞佛偈",       @"url":@"赞佛偈.sutra"}];
-    [_Ke addObject:@{@"name":@"念圣号",       @"url":@"念圣号.sutra"}];
-    [_Ke addObject:@{@"name":@"发愿文",       @"url":@"发愿文普贤十大愿王.sutra"}];
-    [_Ke addObject:@{@"name":@"净土文",       @"url":@"发愿文慈云净土.sutra"}];
-    [_Ke addObject:@{@"name":@"三皈依",       @"url":@"三皈依.sutra"}];
-    [_Ke addObject:@{@"name":@"祝韦天",       @"url":@"韦天赞.sutra"}];
-    [_Ke addObject:@{@"name":@"赞观音文",       @"url":@"赞观音文.sutra"}];
-    [_Ke addObject:@{@"name":@"香赞",        @"url":@"香赞.sutra"}];
-    [_Ke addObject:@{@"name":@"阿弥陀经",       @"url":@"课阿弥陀经.sutra"}];
-    [_Ke addObject:@{@"name":@"大忏悔文",     @"url":@"礼佛大忏悔文.sutra"}];
-    [_Ke addObject:@{@"name":@"蒙山施食",   @"url":@"蒙山施食.sutra"}];
-    [_Ke addObject:@{@"name":@"往生咒",         @"url":@"往生咒.sutra"}];
-    [_Ke addObject:@{@"name":@"观闻圣号",       @"url":@"观闻圣号.sutra"}];
-    [_Ke addObject:@{@"name":@"净土文",       @"url":@"发愿文慈云净土.sutra"}];
-    [_Ke addObject:@{@"name":@"伽蓝赞",       @"url":@"伽蓝赞.sutra"}];
-    
     
     _KeAlbum= [[NSMutableArray alloc] init];
+    
+    NSMutableArray *morningSongList = [NSMutableArray arrayWithObjects:
+                                       @"", nil];
+    
+    
+    KeAlbum *morningSong ;
 }
 
 - (void)didReceiveMemoryWarning {
