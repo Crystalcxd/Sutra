a94ed89a3d2da7d6a934fd9fc1fe2db2613e5d34
Xiaodong <Micheal@sleepace.net>
2017-11-23 22:39:57 +0800

    音乐播放

--- Sutra/AppDelegate.m
+++ Sutra/AppDelegate.m
@@ -45,12 +45,6 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    //开启后台处理多媒体事件
-    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
-    AVAudioSession *session=[AVAudioSession sharedInstance];
-    [session setActive:YES error:nil];
-    //后台播放
-    [session setCategory:AVAudioSessionCategoryPlayback error:nil];
 }
 
 
@@ -67,6 +61,12 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    //开启后台处理多媒体事件
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    AVAudioSession *session=[AVAudioSession sharedInstance];
+    [session setActive:YES error:nil];
+    //后台播放
+    [session setCategory:AVAudioSessionCategoryPlayback error:nil];
 }
 
 
